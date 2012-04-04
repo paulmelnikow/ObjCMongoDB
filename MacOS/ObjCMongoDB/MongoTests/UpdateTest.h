e255ad7ebc4e6845e3980dbfa589e034b9087c36
Paul Melnikow <paulmelnikow@midstatespring.com>
2012-04-04 14:12:33 -0400

    Add test case for update

---
+++ MacOS/ObjCMongoDB/MongoTests/UpdateTest.h
@@ -0,0 +1,15 @@
+//
+//  UpdateTest.h
+//  ObjCMongoDB
+//
+//  Created by Paul Melnikow on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+//  Application unit tests contain unit test code that must be injected into an application to run correctly.
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UpdateTest : SenTestCase
+
+@end
---
+++ MacOS/ObjCMongoDB/MongoTests/UpdateTest.m
@@ -0,0 +1,17 @@
+//
+//  UpdateTest.m
+//  ObjCMongoDB
+//
+//  Created by Paul Melnikow on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UpdateTest.h"
+
+#import <Cocoa/Cocoa.h>
+
+@implementation UpdateTest
+
+
+
+@end
