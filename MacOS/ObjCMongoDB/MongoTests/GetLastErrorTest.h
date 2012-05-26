37237d60eddd3fa829c414e36c4328aa46a13bdb
Paul Melnikow <paulmelnikow@midstatespring.com>
2012-05-26 12:25:30 -0400

    Remove old unused files and sample code and insert license banners

--- MacOS/ObjCMongoDB/MongoTests/GetLastErrorTest.h
+++ MacOS/ObjCMongoDB/MongoTests/GetLastErrorTest.h
@@ -2,11 +2,20 @@
 //  GetLastErrorTest.h
 //  ObjCMongoDB
 //
-//  Created by Paul Melnikow on 3/18/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright 2012 Paul Melnikow and other contributors
 //
-
-//  Logic unit tests contain unit test code that is designed to be linked into an independent test executable.
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 #import <SenTestingKit/SenTestingKit.h>
 #import "MongoConnection.h"
