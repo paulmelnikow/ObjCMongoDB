84eaf7c0b5cb01073a057334688e2f21ab358c2f
Paul Melnikow <email@paulmelnikow.com>
2013-02-03 22:25:38 -0500

    Fix banners

---
+++ Mongo/NSArray+MongoAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+MongoAdditions.h
+//  ObjCMongoDB
+//
+//  Created by Paul Melnikow on 2/3/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (MongoAdditions)
+
+@end
---
+++ Mongo/NSArray+MongoAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+MongoAdditions.m
+//  ObjCMongoDB
+//
+//  Created by Paul Melnikow on 2/3/13.
+//
+//
+
+#import "NSArray+MongoAdditions.h"
+
+@implementation NSArray (MongoAdditions)
+
+@end
